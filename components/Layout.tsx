576e9dc8e8ef4c14a726905ed51507f1bdf07b08
Justin Hilliard <justin.a.hilliard@gmail.com>
2022-05-23 15:01:14 -0700

    initial commit

---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import Meta from './meta'
+
+export default function Layout({ children }: any) {
+    return (
+      <>
+        <Meta />
+        <div className="">
+          <main>{children}</main>
+        </div>
+        {/* <Footer /> */}
+      </>
+    )
+  }
